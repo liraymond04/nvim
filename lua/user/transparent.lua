17a23ab6e1bf8ea6f5cc4f1520aa4a35d00ee280
Raymond Li <li.raymond04@gmail.com>
2023-03-24 12:43:57 -0700

    Update transparent.nvim

--- lua/user/transparent.lua
+++ lua/user/transparent.lua
@@ -2,13 +2,13 @@ local status_ok, transparent = pcall(require, "transparent")
 if not status_ok then
   return
 end
+vim.g.transparent_enabled = true
 transparent.setup({
-    enable = true, -- boolean: enable transparent
     extra_groups = { -- table/string: additional groups that should be cleared
       -- In particular, when you set it to 'all', that means all available groups
   
       -- example of akinsho/nvim-bufferline.lua
       "all",
     },
-    exclude = {}, -- table: groups you don't want to clear
+    exclude_groups = {}, -- table: groups you don't want to clear
   })
