8ace67c902f92c83ee0bf2e3c77c9d214e9ffcb3
liraymond04 <li.raymond04@gmail.com>
2023-07-15 11:00:36 -0600

    Add mappings and plugins Change statusline separator style

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,4 +1,22 @@
 ---@type ChadrcConfig 
- local M = {}
- M.ui = {theme = 'onedark'}
- return M
+local M = {}
+
+M.ui = {
+    theme = 'onedark',
+
+    statusline = {
+        separator_style = "block",
+        overriden_modules = nil,
+    },
+
+    tabufline = {
+        lazyload = true,
+        overriden_modules = nil,
+    },
+}
+
+M.plugins = "custom.plugins"
+
+M.mappings = require "custom.mappings"
+
+return M
