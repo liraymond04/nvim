2a57ae8a7bc70310294dd2f2beb718a7ca3d7f45
liraymond04 <li.raymond04@gmail.com>
2024-02-26 20:57:47 -0800

    Add telescope-fzf-native plugin

--- lua/plugins/config/telescope.lua
+++ lua/plugins/config/telescope.lua
@@ -87,6 +87,13 @@ telescope.setup {
     -- builtin picker
   },
   extensions = {
+    fzf = {
+      fuzzy = true,                   -- false will only do exact matching
+      override_generic_sorter = true, -- override the generic sorter
+      override_file_sorter = true,    -- override the file sorter
+      case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+      -- the default case_mode is "smart_case"
+    },
     ["ui-select"] = {
       require("telescope.themes").get_dropdown {
         -- even more opts
@@ -100,4 +107,5 @@ telescope.setup {
   },
 }
 
+telescope.load_extension("fzf")
 telescope.load_extension("ui-select")
