c6b9d25d8d9ec002a7aa2c2017f6e5e0aef1f9cf
Raymond Li <li.raymond04@gmail.com>
2024-07-16 00:02:52 -0600

    Add peek.nvim for Markdown viewing

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -183,6 +183,16 @@ local plugins = {
   {
     "pteroctopus/faster.nvim",
   },
+  { -- requires webkit2gtk
+    "toppair/peek.nvim",
+    event = { "VeryLazy" },
+    build = "deno task --quiet build:fast",
+    config = function()
+      require("peek").setup()
+      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    end,
+  },
 }
 
 return plugins
