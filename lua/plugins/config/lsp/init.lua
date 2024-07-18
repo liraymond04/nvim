a02dd708a5401b861d6001946745e0d920c98a4d
liraymond04 <li.raymond04@gmail.com>
2024-07-18 16:58:07 -0600

    Fix LSP highlight groups

--- lua/plugins/config/lsp/init.lua
+++ lua/plugins/config/lsp/init.lua
@@ -126,3 +126,4 @@ require(lsp_path .. "null-ls")
 
 -- set highlight LSP highlight groups
 vim.api.nvim_set_hl(0, "LspReferenceText", { blend = 100, underline = true })
+vim.api.nvim_set_hl(0, "LspReferenceRead", { blend = 100, underline = true })
